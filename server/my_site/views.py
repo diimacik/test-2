cae25d4849d47579e7e4a3d2e9cfc447d03887d2
Dmytro <gagamdima534@gmail.com>
2024-04-11 20:43:04 +0200

    add style and aboud page

--- server/my_site/views.py
+++ server/my_site/views.py
@@ -5,3 +5,6 @@ from .models import NewsAdmin
 def index(request):
     news = NewsAdmin.objects.all()
     return render (request, 'index.html', {'news':news})
+
+def about(request):
+    return render (request, 'about.html')
