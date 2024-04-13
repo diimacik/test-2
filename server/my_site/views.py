ef8fb22fb069651dff2e456ef662fd192f133fe0
Dmytro <gagamdima534@gmail.com>
2024-04-13 16:53:54 +0200

    add pk for every news

--- server/my_site/views.py
+++ server/my_site/views.py
@@ -9,6 +9,10 @@ def index(request):
     news = NewsAdmin.objects.all()
     return render (request, 'index.html', {'news':news})
 
+def NewsPost(request, pk):
+    news = NewsAdmin.objects.get(id=pk)
+    return render (request, 'news-admin.html', {'news':news})
+
 def about(request):
     return render (request, 'about.html')
 
